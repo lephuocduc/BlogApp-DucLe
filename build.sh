d69390e5c0bc0ecc4bcc176679a58b86d73002a3
Duc Le <leducst1@gmail.com>
2024-11-08 08:22:05 +0000

    m

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
+mkdir -p /kaniko/.dockerconfig && ln -s /kaniko/.docker/.dockerconfigjson /kaniko/.dockerconfig/config.json
 IMAGE_ID=$1 && \
 IMAGE_TAG=$2 && \
-export DOCKER_CONFIG=/kaniko/.dockerconfig && \
+export DOCKER_CONFIG=/kaniko/.dockerconfigjson && \
 /kaniko/executor \
- --context $(pwd) \
- --dockerfile $(pwd)/Dockerfile \
- --destination $IMAGE_ID:$IMAGE_TAG \
- --force
+  --context $(pwd) \
+  --dockerfile $(pwd)/Dockerfile \
+  --destination $IMAGE_ID:$IMAGE_TAG \
+  --force
