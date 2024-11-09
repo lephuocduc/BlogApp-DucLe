10f5b7cb1311674d93415092a980a23d2f4ffbc6
Duc Le <leducst1@gmail.com>
2024-11-09 03:29:07 +0000

    m

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
-apk add --no-cache openssl
-apk add --no-cache gcc musl-dev linux-headers
+#!/bin/sh
+set -e
+
+# Install necessary packages including OpenSSL
+apk add --no-cache openssl gcc musl-dev linux-headers
+
+# Set environment variables
+export FLASK_APP=app.py
+export FLASK_RUN_HOST=0.0.0.0
 mkdir -p /kaniko/.dockerconfig && ln -s /kaniko/.docker/.dockerconfigjson /kaniko/.dockerconfig/config.json
 IMAGE_ID=$1 && \
 IMAGE_TAG=$2 && \
